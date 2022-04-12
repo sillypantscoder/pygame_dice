c07885ee8ca3d44231534a891e2c9e66842a3e21
Jason Roman <roman.jason29@svvsd.org>
2022-04-12 13:54:52 -0600

    Added dice

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
 
-WHITE = (255, 255, 255)
+pygame.font.init()
 
-screen = pygame.display.set_mode((1, 1))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DICE = [1]
+ZOOM = 100
+FONT = pygame.font.SysFont("monospace", int(ZOOM / 2))
+
+screen = pygame.display.set_mode((len(DICE) * ZOOM, ZOOM))
 
 c = pygame.time.Clock()
 running = True
@@ -10,6 +16,13 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	# SCREEN
 	screen.fill(WHITE)
+	for i in DICE:
+		pygame.draw.rect(screen, BLACK, pygame.Rect(10, 10, 80, 80), 0, 20)
+		text = "1"
+		rendered = FONT.render(text, True, WHITE)
+		screen.blit(rendered, (50 - (rendered.get_width() / 2), 50 - (rendered.get_height() / 2)))
+	# FLIP
 	pygame.display.flip()
 	c.tick(60)
