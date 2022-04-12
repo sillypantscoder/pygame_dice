79454f02769c5abe32fa23731f785912803d3cd0
Jason Roman <roman.jason29@svvsd.org>
2022-04-12 14:09:19 -0600

    Dice and total text zoom in

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ BLACK = (0, 0, 0)
 DICE = [1, 2, 3, 4, 5, 6]
 ZOOM = 100
 FONT = pygame.font.SysFont("monospace", int(ZOOM / 2))
+FONTHEIGHT = FONT.render("0", True, BLACK).get_height()
+DICE_HEIGHT = 0
+TEXTHEIGHT = 0
 
-screen = pygame.display.set_mode((len(DICE) * ZOOM, ZOOM))
+screen = pygame.display.set_mode((len(DICE) * ZOOM, ZOOM + FONTHEIGHT))
 
 def reroll():
 	global DICE
+	global DICE_HEIGHT
+	global TEXTHEIGHT
 	DICE = random.choices([1, 2, 3, 4, 5, 6], k=len(DICE))
+	DICE_HEIGHT = -ZOOM
+	TEXTHEIGHT = FONTHEIGHT
 
 reroll()
 
@@ -29,10 +36,14 @@ while running:
 	screen.fill(WHITE)
 	x = 0
 	for i in DICE:
-		pygame.draw.rect(screen, BLACK, pygame.Rect(x + 10, 10, 80, 80), 0, 20)
+		pygame.draw.rect(screen, BLACK, pygame.Rect(x + 10, 10 + DICE_HEIGHT, 80, 80), 0, 20)
 		rendered = FONT.render(str(i), True, WHITE)
-		screen.blit(rendered, ((50 + x) - (rendered.get_width() / 2), 50 - (rendered.get_height() / 2)))
+		screen.blit(rendered, ((50 + x) - (rendered.get_width() / 2), (50 + DICE_HEIGHT) - (rendered.get_height() / 2)))
 		x += 100
+	DICE_HEIGHT /= 1.1
+	if DICE_HEIGHT >= -1: TEXTHEIGHT /= 1.6
+	rendered = FONT.render("Total: " + str(sum(DICE)), True, BLACK)
+	screen.blit(rendered, (0, 100 + TEXTHEIGHT))
 	# FLIP
 	pygame.display.flip()
 	c.tick(60)
